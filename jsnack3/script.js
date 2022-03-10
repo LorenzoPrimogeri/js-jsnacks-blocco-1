e657d6b847c48ae369bcd74dee5fb6f3eb1268cd
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-10 16:21:40 +0100

    completed

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,8 +1,9 @@
-const numero = prompt("inserisci un numero");
 let somma = 0;
-somma = Math.floor((numero / 1) % 10);
-somma += Math.floor((numero / 10) % 10);
-somma += Math.floor((numero / 100) % 10);
-somma += Math.floor((numero / 1000) % 10);
+let numero = prompt("inserisci un numero");
+let myFunc = num => Number(num);
+var intArr = Array.from(String(numero), myFunc);
+for (let i = 0; i < intArr.length; i++) {
+    somma += intArr[i];
+}
+console.log("la somma è: ", somma);
 
-console.log("somma: ", somma);
