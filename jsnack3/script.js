2ef01ced52fc5328d50fc97e2e78220a52d55957
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-10 19:15:04 +0100

    complete

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,8 +4,8 @@ for (let i = 0; i < numero.length; i++) {
     somma += parseInt(numero[i]);
 }
 console.log("La somma è: ", somma);
+
 /* soluzione alternativa
-//12345
 let somma = 0;
 let numero = prompt("inserisci un numero");
 let intArr = Array.from(String(numero), Number);
@@ -13,9 +13,9 @@ for (let i = 0; i < intArr.length; i++) {
     somma += intArr[i];
 }
 console.log("la somma è: ", somma);
-23
-Soluzione solo per n numeri definiti (in questo caso 4)
-somma = Math.floor((numero / 1) % 10); //3
+
+Soluzione solo per n numeri definiti 
+somma = Math.floor((numero / 1) % 10); 
 somma += Math.floor((numero / 10) % 10);
 somma += Math.floor((numero / 100) % 10);
 somma += Math.floor((numero / 1000) % 10);
